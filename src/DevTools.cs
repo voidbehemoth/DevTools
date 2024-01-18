f74018ce0477a9cd08e4be283a823e9f1d794959
VoidBehemoth <VoidBehemoth@gmail.com>
2024-01-18 13:08:11 -0700

    Create boilerplate code.

---
+++ src/DevTools.cs
@@ -0,0 +1,13 @@
+﻿using SML;
+using System;
+
+namespace DevTools;
+
+[Mod.SalemMod]
+public class ExampleMod
+{
+    public static void Start()
+    {
+        Console.WriteLine("DevTools works!");
+    }
+}
