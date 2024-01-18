011aeaa7c3d70c064a0a0bdb8d8e9a4366b4c204
VoidBehemoth <voidbehemoth@gmail.com>
2024-01-18 14:07:16 -0700

    Basic functionality

--- src/DevTools.cs
+++ src/DevTools.cs
@@ -1,4 +1,7 @@
-﻿using SML;
+﻿using Game;
+using HarmonyLib;
+using Services;
+using SML;
 using System;
 
 namespace DevTools;
@@ -6,8 +9,38 @@ namespace DevTools;
 [Mod.SalemMod]
 public class ExampleMod
 {
-    public static void Start()
-    {
+    public static void Start() {
         Console.WriteLine("DevTools works!");
+    }
+}
+
+[DynamicSettings]
+public class DevToolsDynamicSettings {
+    public ModSettings.CheckboxSetting DebugMode {
+        get {
+            return new ModSettings.CheckboxSetting {
+                Name = "Debug Mode",
+                Description = "Client side only.",
+                DefaultValue = false,
+                Available = true,
+                OnChanged = (bool b) => {
+                    if (!Leo.IsGameScene()) return;
+
+                    int userLevel = b ? 999 : 1;
+
+                    Service.Game.Sim.simulation.userLevel.Set(userLevel);
+                }
+            };
+        }
+    }
+}
+
+[HarmonyPatch(typeof(LocalClient), nameof(LocalClient.Start))]
+public class DebugUI {
+    [HarmonyPostfix]
+    public static void Postfix(LocalClient __instance) {
+        if (__instance.gameNetworkService.LoginInfo == null) return;
+
+        __instance.simulation.userLevel.Set(ModSettings.GetBool("Debug Mode") ? 999 : 1);
     }
 }
